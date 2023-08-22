5aadae9f279f625662ff786d51970adb9889a5dc
tahirivazquez <tahiwdev@gmail.com>
2023-08-21 21:41:35 -0300

    Terminado

--- js/data.js
+++ js/data.js
@@ -1,10 +1,7 @@
-// Obtener referencia al elemento donde se mostrará el dato
 const dataElement = document.getElementById("data");
 
-// Obtener el valor almacenado en localStorage
 const storedValue = localStorage.getItem("userInput");
 
-// Mostrar el valor en el elemento HTML
 if (storedValue) {
   dataElement.textContent = storedValue;
 } else {
